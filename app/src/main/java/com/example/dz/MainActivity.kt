17aabbb48dc0a5cb48014ad8fc1d81aa9acdcb6e
AndrMe <124451118+AndrMe@users.noreply.github.com>
2025-11-02 17:34:44 +0300

    Initial Commit

---
+++ app/src/main/java/com/example/dz/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.dz
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+
+class MainActivity: ComponentActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            Text(
+                text = "text",
+            )
+        }
+    }
+}
